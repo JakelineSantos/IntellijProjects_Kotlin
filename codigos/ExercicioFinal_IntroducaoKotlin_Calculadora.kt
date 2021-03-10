cd9c3a8e689bf7e92700157ee9bb2c5a110a570c
JakelineSantos <79661327+JakelineSantos@users.noreply.github.com>
2021-03-09 22:04:11 -0300

    Update ExercicioFinal_IntroducaoKotlin_Calculadora.kt

--- codigos/ExercicioFinal_IntroducaoKotlin_Calculadora.kt
+++ codigos/ExercicioFinal_IntroducaoKotlin_Calculadora.kt
@@ -1,5 +1,4 @@
 fun main(){
-
        // Substitua os valores das variáveis 'a' e 'b' a seguir por números decimais
        // Para isso, utilize ponto no lugar da vírgula e 'f' no final
        // Substitua o valor da variável 'opção' conforme números a seguir:
@@ -12,21 +11,19 @@ fun main(){
         var b:Float? = 1.01f
 
         if (a != null && b != null) {
-            println("Valores preenchidos corretamente. Aplicando operador...")
-
-               
+              println("Valores preenchidos corretamente. Aplicando operador...")
+              
         	val opcao = 1
-            var resultado:Float? = null
+              var resultado:Float? = null
 
-            when (opcao) {
+              when (opcao) {
             	1 -> resultado = a.plus(b)
-                2 -> resultado = a.minus(b)
-                3 -> resultado = a.div(b)
-                4 -> resultado = a.times(b)
+              2 -> resultado = a.minus(b)
+              3 -> resultado = a.div(b)
+              4 -> resultado = a.times(b)
             	else -> {
-                println("Operador inválido. Substitua o valor de 'opcao' conforme orientações iniciais.")}}
-            println("Resultado da operação: $resultado")
-            
+              println("Operador inválido. Substitua o valor de 'opcao' conforme orientações iniciais.")}}
+              println("Resultado da operação: $resultado")
         } else {
-          println("Erro: valor nulo. Substituia 'a' e 'b' por números válidos.")}
+        println("Erro: valor nulo. Substituia 'a' e 'b' por números válidos.")}
     }
