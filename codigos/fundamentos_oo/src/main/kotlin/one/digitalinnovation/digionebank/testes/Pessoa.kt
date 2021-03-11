2a8ad768930c788d51e54699c4e57faa820a28ef
Jakeline Vituci <jaki_vetuci@hotmail.com>
2021-03-11 10:54:45 -0300

    propriedade private set

--- codigos/fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/testes/Pessoa.kt
+++ codigos/fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/testes/Pessoa.kt
@@ -2,12 +2,15 @@ package one.digitalinnovation.digionebank.testes
 
 class Pessoa {
     var nome: String = "Maria"
+
     var cpf: String = "123.456.789-11"
+    private set
 }
 
 fun main(){
     val maria = Pessoa()
 
+    println(maria)
     println(maria.nome)
     println(maria.cpf)
 }
