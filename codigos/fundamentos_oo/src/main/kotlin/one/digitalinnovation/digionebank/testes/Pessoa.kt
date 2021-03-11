01276e2ea6319a09ab2333d4abffb5c3cea32859
Jakeline Vituci <jaki_vetuci@hotmail.com>
2021-03-11 11:08:46 -0300

    adicionando membros a classe

--- codigos/fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/testes/Pessoa.kt
+++ codigos/fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/testes/Pessoa.kt
@@ -2,15 +2,15 @@ package one.digitalinnovation.digionebank.testes
 
 class Pessoa {
     var nome: String = "Maria"
-
     var cpf: String = "123.456.789-11"
     private set
+
+    constructor()
+
+    fun pessoaInfo() = "$nome e $cpf"
 }
 
 fun main(){
     val maria = Pessoa()
-
-    println(maria)
-    println(maria.nome)
-    println(maria.cpf)
+    println(maria.pessoaInfo())
 }
