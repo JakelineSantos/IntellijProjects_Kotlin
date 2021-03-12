5bab1b678c75afb2eb4df61f361bdc2de1821b8f
Jakeline Vituci <jaki_vetuci@hotmail.com>
2021-03-12 10:47:56 -0300

    Aula Abstração 1

--- codigos/fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ codigos/fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,16 +1,6 @@
 package one.digitalinnovation.digionebank
 
-class Pessoa {
-    var nome: String = "Maria"
-    var cpf: String = "123.456.789-11"
-    private set
-
-    constructor()
-
-    fun pessoaInfo() = "$nome e $cpf"
-}
-
-fun main(){
-    val maria = Pessoa()
-    println(maria.pessoaInfo())
-}
+open class Pessoa (
+    open val nome: String,
+    open val cpf: String
+    )
