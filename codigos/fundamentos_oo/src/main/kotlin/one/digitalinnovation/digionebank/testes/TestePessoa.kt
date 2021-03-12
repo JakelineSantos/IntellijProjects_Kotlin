1ce85cd1bbf8b6d501168a28e4beffb6bd502809
Jakeline Vituci <jaki_vetuci@hotmail.com>
2021-03-12 10:56:17 -0300

    Aula Abstração 2

--- codigos/fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/testes/TestePessoa.kt
+++
@@ -1,9 +0,0 @@
-package one.digitalinnovation.digionebank.testes
-
-import one.digitalinnovation.digionebank.Pessoa
-
-fun main() {
-    val maria = Pessoa(nome = "Maria Antonieta" , cpf = "987.654.321-99")
-    println(maria.nome)
-    println(maria.cpf)
-}
